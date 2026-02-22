ac6cb6d14573fd85df7fa8a41cc32b4389cc1916
Beswin Joe <beswinjo70@gmail.com>
2026-02-22 20:49:56 +0530

    Add conditionals example for age and marks

---
+++ basics/day03_conditionals.py
@@ -0,0 +1,19 @@
+print("Day 03 - Learning Conditionals 🚀")
+
+age = 19
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+marks = 85
+
+if marks >= 90:
+    print("Grade: A+")
+elif marks >= 75:
+    print("Grade: A")
+elif marks >= 60:
+    print("Grade: B")
+else:
+    print("Grade: C")
