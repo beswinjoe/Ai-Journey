62c829708bd8c574cc45e29af52e3a9e81e78e32
Beswin Joe <beswinjo70@gmail.com>
2026-02-22 20:56:57 +0530

    Add examples of for and while loops

---
+++ basics/day04_loops.py
@@ -0,0 +1,14 @@
+print("Day 04 - Learning Loops ")
+
+
+print("For loop example:")
+for i in range(1, 6):
+    print("Number:", i)
+
+
+print("\nWhile loop example:")
+count = 1
+
+while count <= 5:
+    print("Count:", count)
+    count += 1
